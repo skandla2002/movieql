1faacac231a3b5bfb816d12b705675a789039b54
JONGMEONG PAK <skandla2002@gmail.com>
2020-01-19 00:36:33 +0900

    movie db 변경

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,9 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 // js, DB, REST등 어디와도 연결 될 수 있음
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id)
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id)
+    movies: () => getMovies()
   }
 };
 
