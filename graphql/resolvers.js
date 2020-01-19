749d088748b651bb387ca1aa5668970ad3afab77
JONGMEONG PAK <skandla2002@gmail.com>
2020-01-20 06:41:28 +0900

    최초 rating, limit 조건 추가 호출 개선

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,7 +3,7 @@ import { getMovies } from "./db";
 // js, DB, REST등 어디와도 연결 될 수 있음
 const resolvers = {
   Query: {
-    movies: () => getMovies()
+    movies: (_, {rating, limit}) => getMovies(rating, limit)
   }
 };
 
