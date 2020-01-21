c64ff19bc0e2eba4912134d77855e13b5a99f6ee
JONGMEONG PAK <skandla2002@gmail.com>
2020-01-22 01:08:57 +0900

    최종 소스 업데이트

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,11 @@
-import { getMovies } from "./db";
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 // js, DB, REST등 어디와도 연결 될 수 있음
 const resolvers = {
   Query: {
-    movies: (_, {rating, limit}) => getMovies(rating, limit)
+    movies: (_, {rating, limit}) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
   }
 };
 
